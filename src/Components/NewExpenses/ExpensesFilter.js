56403fc8889694a61f98d9750c04be6592854241
ShehanSH <shehanhashen928@gmail.com>
2023-02-06 16:41:06 +0530

    .

--- src/Components/NewExpenses/ExpensesFilter.js
+++ src/Components/NewExpenses/ExpensesFilter.js
@@ -6,13 +6,14 @@ const ExpensesFilter = (props) => {
 
     const dropdownChangeHandler= (event) => {
         props.onChangeFilter(event.target.value)
-        
+        console.log(event.target.value)
     };
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
         <select onChange = {dropdownChangeHandler}>
+        
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
